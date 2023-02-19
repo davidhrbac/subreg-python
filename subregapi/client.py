4325d43abcfadd2257d26d60c3f0893ae55ebbe1
David Hrbáč <david@hrbac.cz>
2023-02-19 15:32:39 +0100

    GET and PUT methods split; introduced all domain functions

--- subregapi/client.py
+++ subregapi/client.py
@@ -13,7 +13,7 @@ class SubregApi:
     def base_url(self):
         return self._base_url
 
-    def _make_request(self, path, params=None):
+    def _get_request(self, path, params=None):
         headers = {"Accept": "application/json", "Authorization": f"Bearer {self.api_key}"}
         url = self.base_url + path
         response = requests.get(url, headers=headers, params=params)
@@ -23,20 +23,74 @@ class SubregApi:
         response.raise_for_status()
         return response.json()
 
+    def _put_request(self, path, data):
+        headers = {"Accept": "application/json", "Authorization": f"Bearer {self.api_key}"}
+        url = self.base_url + path
+        response = requests.put(url, json=data, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
     @property
     def domains_path(self):
         return "domains"
 
-    def check_domain(self, domain=None):
-        if domain:
-            return self._make_request(f"{self.domains_path}/{domain}/check")
-
     def get_domains(self):
-        return self._make_request(self.domains_path)
+        """https://api.demoreg.net/#/Domains/get_domains"""
+        return self._get_request(self.domains_path)
+
+    def check_domain(self, for_user = None):
+        """https://api.demoreg.net/#/Domains/get_domains__domain__check"""
+        params = {"for_user": for_user} if for_user else {}
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}/check", params=params)
+
+    def multicheck_domains(self, domains = None):
+        """https://api.demoreg.net/#/Domains/post_domains_multicheck"""
+        return self._post_request(self.domains_path)
 
     def get_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/get_domains__domain_"""
         if domain:
-            return self._make_request(f"{self.domains_path}/{domain}")
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def create_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/post_domains__domain_"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def set_domains(self):
+        """https://api.demoreg.net/#/Domains/put_domains__domain_"""
+        return self._get_request(self.domains_path)
+
+    def delete_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/delete_domains__domain_"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def renew_domain(self, domain=None):
+        """https://api.demoreg.net/#/domains/post_domains__domain__renew"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def restore_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/post_domains__domain__restore"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def trasfer_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/post_domains__domain__transfer"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def account_transfer_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/post_domains__domain__account_transfer"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
+
+    def autorenew_domain(self, domain=None):
+        """https://api.demoreg.net/#/Domains/put_domains__domain__autorenew"""
+        if domain:
+            return self._get_request(f"{self.domains_path}/{domain}")
 
     @property
     def create_domain_path(self):
@@ -51,8 +105,7 @@ class SubregApi:
             "dns": dns,
             "private_whois": private_whois,
         }
-        return self._make_request(self.create_domain_path, data)
-
+        return self._get_request(self.create_domain_path, data)
 
     @property
     def dns_path(self):
@@ -60,25 +113,28 @@ class SubregApi:
 
     def analyze_dnsrecords(self, domain=None, type=None):
         params = {"dnstype": type} if type else {}
-        return self._make_request(f"{self.dns_path}/{domain}/analyze", params=params)
+        return self._get_request(f"{self.dns_path}/{domain}/analyze", params=params)
 
     def get_dnsrecords(self, domain=None):
-        return self._make_request(f"{self.dns_path}/{domain}")
+        return self._get_request(f"{self.dns_path}/{domain}")
 
     @property
     def contacts_path(self):
         return "contacts"
 
     def get_contact(self, id=None):
-        return self._make_request(f"{self.contacts_path}/{id}")
+        return self._get_request(f"{self.contacts_path}/{id}")
 
     def get_contacts(self):
-        return self._make_request(self.contacts_path)
+        return self._get_request(self.contacts_path)
+
+    def set_contact(self, id=None, data=None):
+        return self._put_request(f"{self.contacts_path}/{id}", data)
 
     @property
     def create_contact_path(self):
         return "domain/create-contact"
 
     def create_contact(self, contact):
-        return self._make_request(self.create_contact_path, contact)
+        return self._get_request(self.create_contact_path, contact)
 
